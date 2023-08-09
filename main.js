313bcad5f1482a40aa3deb2a41667ae3c80dd99f
Ruud0511 <post@ruud.co>
2023-08-09 17:54:23 +0200

    controle javascript en aanpassingen gedaan

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
-//
-// function handleUsername() {
-//     console.log('Ik heb een gebruikersnaam ingevoerd')
-// }
-//
-// const username = document.getElementById('username-field');
-//
-// username.addEventListener('click', handleUsername);
+// Referenties inputvelden //
+const usernameInput = document.getElementById('username-field');
+const passwordInput = document.getElementById('password-field')
 
-// 1. username //
-function handleInputUsername (e) {
-    console.log(e.target.value);
+// Referenties form warnings //
+const usernameWarning = document.getElementById('username-warning');
+const passwordWarning = document.getElementById('password-warning');
+
+// Eventlisteners //
+usernameInput.addEventListener('keyup', checkUsername);
+passwordInput.addEventListener('keyup', checkPassword);
+
+// Functie username input en aanroep door eventlistener //
+
+function checkUsername(e) {
+    if (e.target.value.includes('@')) {
+        usernameWarning.textContent = 'Gebruikersnaam mag geen @ bevatten';
+    }
+    else {
+        usernameWarning.textContent = ''
+    }
 }
 
-const usernameInputField = document.getElementById('username-field');
 
-usernameInputField.addEventListener('keyup', handleInputUsername);
+// Functie password en aanroep door event listener //
 
-// console.log('Gebruikersnaam mag geen @ bevatten')
-
-
-// 2. password //
-function handleInputPassword (e) {
-    console.log(e.target.value);
+function checkPassword(e) {
+    if (e.target.value.length >= 6) {
+        passwordWarning.textContent = '';
+    }
+    else {
+        passwordWarning.textContent = 'Wachtwoord is te kort, gebruik minimaal 6 tekens';
+    }
 }
-
-const passwordInputField = document.getElementById('password-field');
-
-passwordInputField.addEventListener('keyup', handleInputPassword);
